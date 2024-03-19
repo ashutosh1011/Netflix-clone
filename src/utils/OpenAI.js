368eea944af2701ec6d195ae10f1a3d808418f96
Ashutosh Tripathi <ramtripathi246@gmail.com>
2024-03-19 21:55:30 +0530

    Update OpenAI API key and add .env to .gitignore

--- src/utils/OpenAI.js
+++ src/utils/OpenAI.js
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import { GPT_KEY } from "./Common";
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: GPT_KEY,
   dangerouslyAllowBrowser: true,
 });
 
